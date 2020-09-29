eac5875ea4c766fd2f48428a9612cd5febb61c0a
Arnold <arnoldingunza@gmail.com>
2020-09-29 02:51:22 -0500

    modifique el post

--- src/main/java/com/computacionfisica/api/controllers/RegistroCovidRestController.java
+++ src/main/java/com/computacionfisica/api/controllers/RegistroCovidRestController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,13 +33,17 @@ public class RegistroCovidRestController {
 		return registroCovidService.findAll();
 	}
 	
-	@PostMapping("/mostrar/{temperatura}")
-	public ResponseEntity<?> mostrar(@PathVariable float temperatura) {
+	@PostMapping("/temperatura")
+	public ResponseEntity<?> mostrar(@RequestBody Map<String, Object> temperatura) {
 		
 		Map<String, Object> response = new HashMap<>();
 		List<Object[]> registroLast;
+		float temperatura1 = (float) 0.6;
+		String cadena=String.valueOf(temperatura.get("temperatura"));
+		float temperaturafloat = Float.parseFloat(cadena);
+		System.out.println(temperaturafloat);
 		try {
-			 registroLast = registroCovidService.mostrar(temperatura);
+			 registroLast = registroCovidService.mostrar(temperaturafloat);
 		} catch(DataAccessException e) {
 			response.put("mensaje", "Error al realizar la consulta en la base de datos");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
